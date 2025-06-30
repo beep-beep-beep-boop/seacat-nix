e848b5a14488be4459beeb9442e06ebc2ad790cd
beep beep beep boop <b@bbbb.foo>
2025-06-29 23:21:01 -0400

    meowmeow

--- nixos/services/icecast.nix
+++ nixos/services/icecast.nix
@@ -1,15 +1,19 @@
 { config, lib, ... }:
 let
     icecastInternalPort = 9002;
+    icecastHostname = "iwanttolistentomusic.ilovelain.com";
 in
 {
     services.icecast = {
         enable = true;
         listen.address = "127.0.0.1";
         listen.port = icecastInternalPort;
+        extraConf = ''
+            <hostname>${icecastHostname}</hostname>
+        '';
     };
 
-    services.caddy.virtualHosts."iwanttolistentomusic.ilovelain.com".extraConfig = ''
+    services.caddy.virtualHosts."${icecastHostname}".extraConfig = ''
         reverse_proxy 127.0.0.1:${toString icecastInternalPort} {
             flush_interval -1
         }
