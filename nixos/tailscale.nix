ff3ec1c07487f4dfd13f6861ca6deb49022788fd
beep beep beep boop <b@bbbb.foo>
2025-06-30 01:09:36 -0400

    meow

--- nixos/tailscale.nix
+++ nixos/tailscale.nix
@@ -1,6 +1,10 @@
 { config, lib, ... }:
 {
     services.tailscale.enable = true;
+    
+    # make the tailscale command available
+    environment.systemPackages = [ pkgs.tailscale ];
+
     networking.firewall = {
         trustedInterfaces = [ "tailscale0" ];
         allowedUDPPorts = [ config.services.tailscale.port ];
